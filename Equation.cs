02663ad78284df192cc5eb1d40e71d985671ea53
DaksEr <danchick53@gmail.com>
2024-06-28 17:35:55 +0500

    Добавьте файлы проекта.

---
+++ Equation.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equations
+{
+    internal class Equation
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Equation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double Discriminant()
+        {
+            return b * b - 4 * a * c;
+        }
+
+        public double[] Result()
+        {
+            double d = this.Discriminant();
+
+            if (d == 0)
+            {
+                double[] res = new double[1];
+                res[0] = (-1 * b) / (2 * a);
+                Console.WriteLine("Корень уравнения равен " + res[0]);
+                return res;
+            }
+            else if (d > 0)
+            {
+                double[] res = new double[2];
+                res[0] = (-b + Math.Sqrt(d)) / (2 * a);
+                res[1] = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("Корни уравнения равны " + res[0] + " и " + res[1]);
+                return res;
+            }
+            else
+            {
+                double[] res = new double[0];
+                Console.WriteLine("Корней не существуют");
+                return res;
+            }
+        }
+
+        public static Equation operator ++(Equation x)
+        {
+            return new Equation(x.a + 1, x.b + 1, x.c + 1);
+        }
+
+        public static Equation operator --(Equation x)
+        {
+            return new Equation(x.a - 1, x.b - 1, x.c - 1);
+        }
+
+        public static implicit operator double(Equation x)
+        {
+            return x.Discriminant();
+        }
+
+        public static explicit operator bool(Equation x)
+        {
+            return x.Discriminant() >= 0;
+        }
+
+        public static bool operator ==(Equation x, Equation y)
+        {
+            return x.a == y.a && x.b == y.b && x.c == y.c;
+        }
+
+        public static bool operator !=(Equation x, Equation y)
+        {
+            return !(x.a == y.a && x.b == y.b && x.c == y.c);
+        }
+
+        public override string ToString()
+        {
+            return "(a = " + a + "; b = " + b + "; c = " + c + ")";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Equations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation equation1 = new Equation(2, 5, 2);
+            Equation equation2 = new Equation(1, 2, 3);
+            Console.WriteLine("Коэффициенты 1-го уравнения: " + equation1.ToString());
+            equation1.Result();
+            ++equation1;
+            Console.WriteLine("Увеличение коэффициентов на 1: " + equation1.ToString());
+            --equation1;
+            Console.WriteLine("Уменьшение коэффициентов на 1: " + equation1.ToString());
+            Console.WriteLine("Дискриминант уравнения равен " + (double)equation1);
+            Console.WriteLine("Корни уравнения существуют: " + (bool)equation1);
+            Console.WriteLine("Коэффициенты 2-го уравнения: " + equation2.ToString());
+            Console.WriteLine("Уравнения равны: " + (equation1 == equation2));
+            Console.WriteLine("Уравнения не равны: " + (equation1 != equation2));
+        }
+    }
+}
